06eb8d18e1aae57af8db2e8e827b877fa090d3cf
Ryan Howard <ryanhoward810@gmail.com>
2017-11-02 13:57:48 -0700

    committing changes

---
+++ keys.js
@@ -0,0 +1,22 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+	consumer_key: "luVdtCk0f5ajjJLD1xxRl0739",
+	consumer_secret: "8qRiDkq0DlCTG5lXea6u1VXoEPis5XZxqtmj653pGVDpwfNXTM",
+	access_token_key: "925861475385729024-V7OjnzRKFaihedoVkupRwsT1km2TPHV",
+	access_token_secret: "jFDTzmQATT840I5l3zlUdWVhdXlZ5iW63mliQrRS4gTFg",
+
+
+
+}
+
+module.exports = twitterKeys;
+
+
+
+
+// Spotify...
+// Client ID
+// a04d3518f3394a37b7d8348e0cc2bae2
+// Client Secret
+// 86687cb18301434a8096e8866ad4a93a
